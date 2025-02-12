7bd00cea7b447e87b986bb97710d169623c2899f
Tuấn Thịnh <ltthinh111@gmail.com>
2025-02-12 12:36:42 +0700

    DONE bai5

--- fe/fruitshop/src/pages/users/theme/header/index.js
+++ fe/fruitshop/src/pages/users/theme/header/index.js
@@ -1,12 +1,66 @@
 import { memo } from "react";
-import './style.scss';
-
+import "./style.scss";
+import {
+    AiOutlineFacebook,
+    AiOutlineInstagram,
+    AiOutlineLinkedin,
+    AiOutlineGithub,
+    AiOutlineUser,
+    AiOutlineMail,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { formatter } from "utils/formatter";  
 const Header = () => {
     return (
         <div className="header_top">
-            <div className="container">Header</div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 header_top_left">
+                        <ul>
+                            <li>
+                                <AiOutlineMail />
+                                ltthinh111@gmail.com
+                            </li>
+                            <li>
+                                Miễn phí giao hàng cho đơn hàng trên {formatter(200000)}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-6 header_top_right">
+                        <ul>
+                            <li>
+                                <Link to={''}>
+                                    <AiOutlineFacebook />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={''}>
+                                    <AiOutlineInstagram />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={''}>
+                                    <AiOutlineLinkedin />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={''}>
+                                    <AiOutlineGithub />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={''}>
+                                    <AiOutlineUser />
+                                </Link>
+                                <span>Đăng nhập</span>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default memo(Header) ;
+export default memo(Header);
